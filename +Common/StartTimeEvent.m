416ae8747b2b4d647fd6af27c800441f3cffdd88
kndiaye <karim.ndiaye@upmc.fr>
2023-10-03 11:02:14 +0200

    Keep Cursor  in FastDebug mode

--- +Common/StartTimeEvent.m
+++ +Common/StartTimeEvent.m
@@ -5,7 +5,7 @@ switch S.OperationMode
     case 'Acquisition'
         HideCursor(S.ScreenID)
     case 'FastDebug'
-        HideCursor(S.ScreenID)
+        % HideCursor(S.ScreenID)
     case 'RealisticDebug'
         HideCursor(S.ScreenID)
     otherwise
